0874d4faa98c0f8c8de82f7cf8d0d7444143204e
Emilio Bernal <ebernalzubieta@gmail.com>
2022-11-02 17:54:53 -0600

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://xilonet-signa.netlify.app/", "http://localhost:5173/"
+    origins "*"
 
     resource "*",
       headers: :any,
